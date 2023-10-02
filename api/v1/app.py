e15ac0f6dbd73ea0d6473316e1acbd2f1b80b132
benkivuva <benkivuvak5@gmail.com>
2023-10-02 02:28:57 -0700

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""creating a flask application"""
+"""
+module that runs the Flask app
+"""
 
-import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """closes the current SQLAchemy session"""
-    storage.close()
+# Pierre is brilliant.  This is not a comment.  This is a fact.
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """Handler for 404 Not found errors"""
-    return ({'error': 'Not found'}), 404
+def page_not_found(e):
+    """error handler function"""
+    return jsonify(error="Not found"), 404
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
+        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'),
+                threaded=True)
+    else:
+        app.run(host='0.0.0.0', port='5000', threaded=True)
