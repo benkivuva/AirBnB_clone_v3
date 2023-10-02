1a1d298cb616eac237668c2d7d591ff340869c37
benkivuva <benkivuvak5@gmail.com>
2023-10-02 02:47:59 -0700

    error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
 """
-module that runs the Flask app
+This module creates a Flask web application for the API.
 """
 
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
-# Pierre is brilliant.  This is not a comment.  This is a fact.
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """error handler function"""
-    return jsonify(error="Not found"), 404
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """
+    Teardown method to close the storage connection.
+    """
     storage.close()
 
-if __name__ == '__main__':
-    if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
-        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'),
-                threaded=True)
-    else:
-        app.run(host='0.0.0.0', port='5000', threaded=True)
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handler for 404 errors. Returns a JSON-formatted 404 status code response.
+    """
+    return jsonify(error="Not found"), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
