69479ccccbb1beaba3ec0e27df5c1cab1e35b9a5
benkivuva <benkivuvak5@gmail.com>
2023-10-02 02:35:38 -0700

    correctionvi index.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-this is the __init__.py file
+Package for API endpoints.
 """
-from flask import Blueprint
 
+
+from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Import the views after app_views to avoid circular import
 from api.v1.views.index import *
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_reviews
-from api.v1.views import places_amenities
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This module defines routes for the API status.
 """
-from flask import jsonify
-from models import storage
+
+
 from api.v1.views import app_views
+from flask import jsonify
 
-
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
-    """display the status response"""
+    """
+    Retrieves the status of the API.
+    """
     return jsonify(status="OK")
-
-
-@app_views.route('/stats', strict_slashes=False)
-# This code has the advantage of not returning non-existing objects
-def stats():
-    """display the number of each objects by type"""
-    all_classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
-                   "Review": "reviews", "State": "states", "User": "users"}
-    return jsonify({v: storage.count(k) for k, v in all_classes.items()
-                    if storage.count(k)})
